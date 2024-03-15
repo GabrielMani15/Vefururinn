f68e490861a7eab24be059c4c0765a55e09fcb72
Gabriel Mani <Gabriel.sigvaldason@gmail.com>
2024-03-15 14:52:50 +0000

    Burt

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    litliReikningurinn = (2 + 2) * 2
     return "<h1>Einn góð stafsetingavilla á þessu haha. upsii!</h1>"
 
 if __name__ == "__main__":
